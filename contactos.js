496232ececfedc05b056670c07ff052c34f7a5af
Alicia Aranda <loliaranda29@gmail.com>
2023-09-04 20:15:46 -0300

    modificacion de ubicacion

--- contactos.js
+++ contactos.js
@@ -11,10 +11,10 @@
 
 //Crea una lista de contactos con datos predefinidos, cada contacto debe contener el nombre y apellido como una sola cadena de caracteres
 let contactos = [
-    {'id' : '1', 'nombres' : 'Alicia Adela', 'apellidos' : 'Aranda', 'telefono' : '2615675456', 'ubicaciones' : 'sur', 'ciudad' : 'Luján de Cuyo', 'direccion' : 'Isla Nueva 532'},
-    {'id' : '2', 'nombres' : 'Agustina Ayelen', 'apellidos' : 'Aranda', 'telefono' : '2615129876', 'ubicaciones' : 'sur', 'ciudad' : 'Luján de Cuyo', 'direccion' : 'Isla Nueva 532'},
-    {'id' : '3', 'nombres' : 'Cristian Ariel', 'apellidos' : 'Moreno', 'telefono' : '2613872652', 'ubicaciones' : 'oeste', 'ciudad' : 'Godoy Cruz', 'direccion' : 'Salta 333'},
-    {'id' : '4', 'nombres' : 'Pedro Santino', 'apellidos' : 'Silvestrini', 'telefono' : '2612736984', 'ubicaciones' : 'sur', 'ciudad' : 'Luján de Cuyo', 'direccion' : 'San Martin 3673'},
+    {'id' : '1', 'nombres' : 'Alicia Adela', 'apellidos' : 'Aranda', 'telefono' : '2615675456', 'ubicaciones' : {'ciudad' : 'Luján de Cuyo', 'direccion' : 'Isla Nueva 532'}},
+    {'id' : '2', 'nombres' : 'Agustina Ayelen', 'apellidos' : 'Aranda', 'telefono' : '2615129876', 'ubicaciones': {'ciudad' : 'Luján de Cuyo', 'direccion' : 'Isla Nueva 532'}},
+    {'id' : '3', 'nombres' : 'Cristian Ariel', 'apellidos' : 'Moreno', 'telefono' : '2613872652', 'ubicaciones' : {'ciudad' : 'Godoy Cruz', 'direccion' : 'Salta 333'}},
+    {'id' : '4', 'nombres' : 'Pedro Santino', 'apellidos' : 'Silvestrini', 'telefono' : '2612736984', 'ubicaciones' : {'ciudad' : 'Luján de Cuyo', 'direccion' : 'San Martin 3673'}},
 ];
 
 //Crea una función para añadir un nuevo contacto a una lista
@@ -24,7 +24,7 @@ const agregarContacto = (contacto) =>{
 
 }
 
-agregarContacto({'id' : '5', 'nombres' : 'Daniel Ernesto', 'apellidos' : 'Aranda', 'telefono' : '2614837927', 'ubicaciones' : 'oeste', 'ciudad' : 'Godoy Cruz', 'direccion' : 'Ingenieros 76'})
+agregarContacto({'id' : '5', 'nombres' : 'Daniel Ernesto', 'apellidos' : 'Aranda', 'telefono' : '2614837927', 'ubicaciones' : {'ciudad' : 'Godoy Cruz', 'direccion' : 'Ingenieros 76'}})
 
 console.log(contactos)
             
@@ -36,6 +36,6 @@ const borrarContacto = (contacto) =>{
    
 }
 
-borrarContacto({'id' : '4', 'nombres' : 'Pedro Santino', 'apellidos' : 'Silvestrini', 'telefono' : '2612736984', 'ubicaciones' : 'sur', 'ciudad' : 'Luján de Cuyo', 'direccion' : 'San Martin 3673'})
+borrarContacto({'id' : '4', 'nombres' : 'Pedro Santino', 'apellidos' : 'Silvestrini', 'telefono' : '2612736984', 'ubicaciones' : {'ciudad' : 'Luján de Cuyo', 'direccion' : 'San Martin 3673'}})
 
 console.log(contactos)
